6409cc74e264f1ea4dc8023bbc59f673f8021d94
Evgeniy <anegin_020@mail.ru>
2015-05-16 19:55:28 +0300

    Edit style

--- public/index.js
+++ public/index.js
@@ -1,8 +1,20 @@
+/* Style start */
+$(document).ready(function() {
+    $('.button-info').click(function () {
+        if ($(this).hasClass('active')) {
+            $('.information').css({'display': 'none'});
+            $(this).removeClass('active');
+        }
+        else {
+            $('.information').css({'display': 'block'});
+            $(this).addClass('active');
+        }
+    });
+});
+/* Style start */
+
 var socket = io.connect('http://localhost:3000');
 socket.on('news', function (data) {
     console.log(data);
     socket.emit('my other event', { my: 'data' });
 });
-
-
-
